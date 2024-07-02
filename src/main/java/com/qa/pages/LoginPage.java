b9b8cd2d4d9e64a241e25dea6d92cefd7e7201fc
dumpeta123 <rajendar.dumpeta@gmail.com>
2024-07-02 20:59:27 +0800

    added search page

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -1,10 +1,13 @@
 package com.qa.pages;
 
 public class LoginPage {
-	
-	
+
 	public void loginpage() {
 		System.out.println("Login page");
 	}
 
+	public void loginSet() {
+
+		System.out.println("Login Set");
+	}
 }
